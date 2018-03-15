110b135846451d1b5c80e9b827ce4e0c84daabfa
todaycode <onetoday1030@gmail.com>
2018-03-14 18:38:55 -0700

    Add sign in method constants (#923)
    * Updates Changelog for 4.5.0 release
    * Adds sign-in method constants

--- Firebase/Auth/Source/FIRAuth.m
+++ Firebase/Auth/Source/FIRAuth.m
@@ -532,7 +532,7 @@ static NSMutableDictionary *gKeychainServiceNameForAppName;
     FIRCreateAuthURIRequest *request =
         [[FIRCreateAuthURIRequest alloc] initWithIdentifier:email
                                                 continueURI:@"http://www.google.com/"
-                                       requestConfiguration:_requestConfiguration];
+                                       requestConfiguration:self->_requestConfiguration];
     [FIRAuthBackend createAuthURI:request callback:^(FIRCreateAuthURIResponse *_Nullable response,
                                                      NSError *_Nullable error) {
       if (completion) {
@@ -1123,7 +1123,7 @@ static NSMutableDictionary *gKeychainServiceNameForAppName;
     FIRGetOOBConfirmationCodeRequest *request =
         [FIRGetOOBConfirmationCodeRequest signInWithEmailLinkRequest:email
                                                   actionCodeSettings:actionCodeSettings
-                                                requestConfiguration:_requestConfiguration];
+                                                requestConfiguration:self->_requestConfiguration];
     [FIRAuthBackend getOOBConfirmationCode:request
                                   callback:^(FIRGetOOBConfirmationCodeResponse *_Nullable response,
                                              NSError *_Nullable error) {
